3a82d71a775734427d64c2804492f4d4b5447901
Christian Stein <sormuras@gmail.com>
2024-09-12 17:16:00 +0200

    Update CI build number

--- make/conf/jib-profiles.js
+++ make/conf/jib-profiles.js
@@ -1185,7 +1185,7 @@ var getJibProfilesDependencies = function (input, common) {
             server: "jpg",
             product: "jtreg",
             version: "7.5",
-            build_number: "ci/27",
+            build_number: "ci/28",
             file: "bundles/jtreg-7.5+1.zip",
             environment_name: "JT_HOME",
             environment_path: input.get("jtreg", "home_path") + "/bin",
